cf364238b301f3e08338d1b3e8539c50d2a27885
Cayden Lunt <cayden.lunt@gmail.com>
2025-10-15 21:28:04 -0600

    updated tests.py

--- tests.py
+++ tests.py
@@ -1,37 +1,27 @@
 import pytest
 from sel_tools import *
 
+@pytest.fixture(scope="session")
+def at():
+    return AutoTest()
 
-def test_at():
-    driver = AutoTest.gen_test_driver()
-    at = driver
-    _test_get_text(at)
-    _test_get_value(at)
-    _test_get(at)
+def test_at(at):
+    def test_get_text(at):
+        at.navigate("https://the-internet.herokuapp.com/")
+        txt = at.get_text("//*[@id='content']/h1")
+        assert txt == "Welcome to the-internet"
 
-def _test_get_text(a):
-    a.navigate("https://the-internet.herokuapp.com/")
-    txt = a.get_text("//*[@id='content']/h1")
-    assert txt == "Welcome to the-internet"
+    def test_get_value(at):
+        at.click('//*[@id="content"]/ul/li[27]/a')
+        val1 = at.get_value("//*[@id='content']/div/div/div/input")
+        at.set_value("//*[@id='content']/div/div/div/input", 123)
+        val2 = at.get_value("//*[@id='content']/div/div/div/input")
+        assert val1 == ''
+        assert val2 == '123'
 
-def _test_get_value(a):
-    a.click('//*[@id="content"]/ul/li[27]/a')
-    val1 = a.get_value("//*[@id='content']/div/div/div/input")
-    a.set_value("//*[@id='content']/div/div/div/input", 123)
-    val2 = a.get_value("//*[@id='content']/div/div/div/input")
-    assert val1 == ''
-    assert val2 == '123'
-
-
-def _test_get(a):
-    a.click('//*[@id="content"]/ul/li[27]/a')
-    val1 = a.get_value("//*[@id='content']/div/div/div/input")
-    a.set_value("//*[@id='content']/div/div/div/input", 123)
-    val2 = a.get_value("//*[@id='content']/div/div/div/input")
-    assert val1 == ''
-    assert val2 == '123'
-
-
+    test_get_text(at)
+    test_get_value(at)
+    
 
 # Call the main function that is part of pytest so that the
 # computer will execute the test functions in this file.
